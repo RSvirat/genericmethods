c5f449a3e4c9a5f5e8e6d89bff51ef2080cc0cb2
Sudhakar <37861012+RSvirat@users.noreply.github.com>
2019-04-26 15:33:05 +0530

    thread

---
+++ Thread.java
@@ -0,0 +1,54 @@
+package co.javatt;
+class hi extends Thread{
+	public void run() {
+		for(int i=0; i<5; i++) {
+			System.out.println("hi");
+			try {
+				Thread.sleep(1000);
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+class multithrd extends Thread {
+	public void run() {
+		for(int i=0; i<5; i++) {
+			System.out.println("hello world!...");
+			try {
+				Thread.sleep(1000);
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+class join extends Thread {
+	public void run() {
+		for(int i=0; i<5; i++) {
+			System.out.println("joing the");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+public class multitheard{
+	public static void main(String args[]) {
+		Thread t1 = new multithrd();
+		Thread t2 = new hi();
+		Thread t3 = new join();
+		t2.start();
+		try {
+			t1.join();
+			Thread.sleep(10);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		t1.start();
+		t3.start();
+		
+	}
+}
