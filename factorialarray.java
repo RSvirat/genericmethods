40908aeeb0fef6a08ac81d7adabb7df27d44fcfd
Sudhakar <37861012+RSvirat@users.noreply.github.com>
2019-06-05 11:57:55 +0530

    factorialarray in any number like 1000 and 10000

--- factorialarray.java
+++ factorialarray.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class factorialarray {
@@ -8,15 +9,18 @@ public class factorialarray {
 		int arr[] = new int[len];
 		int temp = 0;
 		long fact = 1;
+		BigInteger f = new BigInteger("1");
 		System.out.print("Enter the array value only "+len+"=");
 		for(int i=0; i<=arr.length-1;i++) {
 			arr[i] = s.nextInt();
 			temp = arr[i];
 			for(int j=1; j<=temp; j++) {
-				fact *=j;
+				f = f.multiply(BigInteger.valueOf(j));
+				/* fact *=j; */
 			}
-			System.out.println("factorial of "+arr[i]+" = "+fact+",");
+			System.out.println("factorial of "+arr[i]+" = "+f+",");
 			fact = 1;
+			f = new BigInteger("1");
 		}
 	}
 }
